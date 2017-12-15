ef0efe140fb43665e8df88ddf74a923ded1b3278
GloomySunday <121055230@qq.com>
2017-12-15 22:57:48 +0800

    Add: GSStability initialized

---
+++ GSStability/GSStability/GSStability.h
@@ -0,0 +1,19 @@
+//
+//  GSStability.h
+//  GSStability
+//
+//  Created by 孟钰丰 on 2017/12/15.
+//  Copyright © 2017年 孟钰丰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GSStability.
+FOUNDATION_EXPORT double GSStabilityVersionNumber;
+
+//! Project version string for GSStability.
+FOUNDATION_EXPORT const unsigned char GSStabilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GSStability/PublicHeader.h>
+
+
