5a9a0a97b2c4a9a8fed1fda15d4fad46073ec8b7
GloomySunday <121055230@qq.com>
2018-02-07 14:51:03 +0800

    Updated: code documents updated

--- Scripts/auto_push.sh
+++ Scripts/auto_push.sh
@@ -8,39 +8,39 @@ if [${Tag_Version} = ""]; then
 	exit 1
 else
 	echo "\nSubmit Verson: ${Tag_Version}\n"
-	File_Path=`echo $(cd .. && pwd)`
-	Code_Path="${File_Path}/Example/Pods"
-	Module_File=`find $File_Path -name *.podspec`
+	File_Path=$(cd `dirname $0`; pwd)
+	Module_Path=${File_Path%/*}
+	Module_File=`find $Module_Path -name *.podspec`
+	Module_Code_Path="${Module_Path}/Example/Pods"
 	Module_Name="${Module_File##*/}"
 	Module_Name="${Module_Name%.*}"
-	
 
 	# 1.更新 jazzy documents
 
-	# echo "Begin Generate jazzy Document....\n"
-	# echo $(cd ${Code_Path}; jazzy -c -a Gloomy.Sunday -u https://github.com/GloomySunday049 -g https://github.com/orgs/Sky-And-Hammer -o ${File_Path}/docs --min-acl internal)
-	# echo "\nGit Commit For Document Updated "
-	# echo $(cd ${File_Path};git add .; git commit -m "Updated: code documents updated")
+	echo "Begin Generate jazzy Document....\n"
+	echo $(cd ${Module_Code_Path}; jazzy -c -a Gloomy.Sunday -u https://github.com/GloomySunday049 -g https://github.com/orgs/Sky-And-Hammer -o ${Module_Path}/docs --min-acl internal)
+	echo "\nGit Commit For Document Updated "
+	echo $(cd ${Module_Path};git add .; git commit -m "Updated: code documents updated")
 
 	# # 2.更新 .podspec 版本号
-	# echo "\nBegin Update Podspec Version...."
-	# echo $(sed -i "" "s/s.version          = '0.6.1'/s.version          = '${Tag_Version}'/g" ${Module_File})
-	# echo "\nGit Commit For Podspec Updated"
-	# echo $(cd ${File_Path};git add .; git commit -m "Updated: Podspec Version Updated")
+	echo "\nBegin Update Podspec Version...."
+	echo $(sed -i "" "s/s.version          = '0.6.1'/s.version          = '${Tag_Version}'/g" ${Module_File})
+	echo "\nGit Commit For Podspec Updated"
+	echo $(cd ${Module_Path};git add .; git commit -m "Updated: Podspec Version Updated")
 
 
 
 	# 3.GIT PUSH & GIT TAG PUSH
 	echo "\nGit Push...."
-	echo $(cd ${File_Path}; git push)
+	echo $(cd ${Module_Path}; git push)
 	echo "\nAdd Tag...."
-	echo $(cd ${File_Path};git tag -a ${Tag_Version} -m 'Release Version For Cocoapods')	
+	echo $(cd ${Module_Path};git tag -a ${Tag_Version} -m 'Release Version For Cocoapods')	
 	echo "\nGit Push Tag...."
-	echo $(cd ${File_Path}; git push origin ${Tag_Version})
+	echo $(cd ${Module_Path}; git push origin ${Tag_Version})
 
-	# 4. Thunk to Cocoapods 
+	# # 4. Thunk to Cocoapods 
 	ehco "\n Cocoapods Push..."
-	echo $(cd ${File_Path}; pod trunk push --verbose ${Module_File})
+	echo $(cd ${Module_Path}; pod trunk push --verbose ${Module_File})
 
 	echo "\nSuccess...."
 fi
