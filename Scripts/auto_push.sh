374c593b6750656e13f17f2e735fa46651b5765b
GloomySunday <121055230@qq.com>
2018-02-09 11:41:57 +0800

    Update: auto.sh updated

--- Scripts/auto_push.sh
+++ Scripts/auto_push.sh
@@ -11,20 +11,21 @@ else
 	File_Path=$(cd `dirname $0`; pwd)
 	Module_Path=${File_Path%/*}
 	Module_File=`find $Module_Path -name *.podspec`
-	Module_Code_Path="${Module_Path}/Example/Pods"
+	Module_Code_Path="${Module_Path}/Example"
 	Module_Name="${Module_File##*/}"
 	Module_Name="${Module_Name%.*}"
+	Old_Version="$(grep -r 's.version* ' GSFoundation.podspec |awk -F = '{print $2}')"
 
 	# 1.更新 jazzy documents
 
 	echo "Begin Generate jazzy Document....\n"
-	echo $(cd ${Module_Code_Path}; jazzy -c -a Gloomy.Sunday -u https://github.com/GloomySunday049 -g https://github.com/orgs/Sky-And-Hammer -o ${Module_Path}/docs --min-acl internal)
+	echo $(cd ${Module_Code_Path}; jazzy -m ${Module_Name} -x -workspace,${Module_Name}.xcworkspace,-scheme,${Module_Name} -c -a Gloomy.Sunday -u https://github.com/GloomySunday049 -g https://github.com/orgs/Sky-And-Hammer -o ${Module_Path}/docs --min-acl internal)
 	echo "\nGit Commit For Document Updated "
 	echo $(cd ${Module_Path};git add .; git commit -m "Updated: code documents updated")
 
 	# # 2.更新 .podspec 版本号
 	echo "\nBegin Update Podspec Version...."
-	echo $(sed -i "" "s/s.version          = '0.6.1'/s.version          = '${Tag_Version}'/g" ${Module_File})
+	echo $(sed -i "" "s/${Old_Version}/ '${Tag_Version}'/g" ${Module_File})
 	echo "\nGit Commit For Podspec Updated"
 	echo $(cd ${Module_Path};git add .; git commit -m "Updated: Podspec Version Updated")
 
